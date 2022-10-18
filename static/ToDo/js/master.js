25e4742fdd8d9ae8cd86a9f680bacd418858c737
Jared <jaredgrossthe@gmail.com>
2022-10-18 11:21:41 -0500

    Trying out new effects

--- static/ToDo/js/master.js
+++ static/ToDo/js/master.js
@@ -1,27 +1,15 @@
-const sideScroll = () => {
-    const listWrapperElement = document.querySelector(".js-listWrapper");
-    const listElement = document.querySelector(".js-list");
-    const progressText = document.querySelector(".js-progressText");
-    const progressLine = document.querySelector(".js-progressLine");
+const toggleables = document.querySelectorAll('.toggleable')
+
+const toggleImage = (e) => {
+  const icon = e.composedPath().find((el) =>  el.classList.contains('toggleable'));
   
-    gsap.to(listElement, {
-      x: () => -(listElement.clientWidth - listWrapperElement.clientWidth),
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".js-section",
-        start: "top top",
-        end: () => `+=${listElement.clientWidth - listWrapperElement.clientWidth}`,
-        scrub: true,
-        pin: true,
-        markers: true,
-        anticipatePin: 1,
-        invalidateOnRefresh: true,
-        onUpdate: self => {
-          progressText.textContent = `${Math.trunc((parseFloat(self.progress.toFixed(2))) * 100)}%`
-          progressLine.style.width = `${Math.trunc((parseFloat(self.progress.toFixed(2))) * 100)}%`
-        }
-      }
-    });
+  if (icon.classList.contains('toggled')) {
+    icon.classList.remove('toggled');
+  } else {
+    icon.classList.add('toggled');
   }
-  
-  sideScroll();
+};
+
+toggleables.forEach((toggleable) => {
+  toggleable.addEventListener('click', toggleImage);
+});
