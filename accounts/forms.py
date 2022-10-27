504a85217dc3064a7ff8fe79a1a580617ef2df40
Jared <jaredgrossthe@gmail.com>
2022-10-27 18:58:01 -0500

    Bug fix

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,3 @@
-from attr import attr
 from django import forms 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
